b97ebc4c8d0d524e7dae54a26ad2e8d26e8a2854
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-23 20:30:42 +0200

    implement client-side routing

---
+++ frontend/static/js/script.js
@@ -0,0 +1,51 @@
+import Dashboard from "./views/Dashboard.js";
+import Post from "./views/Post.js";
+import Settings from "./views/Settings.js";
+
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    { path: "/", view: Dashboard },
+    { path: "/posts", view: Post },
+    { path: "/settings", view: Settings },
+  ];
+
+  // Test each route for potential match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+
+  let match = potentialMatches.find(
+    (potentialMatche) => potentialMatche.isMatch
+  );
+
+  if (!match) {
+    match = {
+      route: route[0],
+      isMatch: true,
+    };
+  }
+
+  const view = new match.route.view();
+  document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+
+  router();
+});
